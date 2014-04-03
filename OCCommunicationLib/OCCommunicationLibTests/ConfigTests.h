d0d12006a300e3164d1b0a4cc4dfc12f5b4ba2e8
Javier Gonzalez <jgonzalez@solidgear.es>
2014-04-03 18:09:15 +0200

    I add a config file to save the tests credentials and url

---
+++ OCCommunicationLib/OCCommunicationLibTests/ConfigTests.h
@@ -0,0 +1,19 @@
+//
+//  ConfigTests.h
+//  ownCloud iOS library
+//
+//  Created by javi on 4/3/14.
+//  Copyright (c) 2014 ownCloud. All rights reserved.
+//
+
+//Your entire server url. ex:https://example.owncloud.com/owncloud/
+#define baseUrl @"https://test1.owncloud.com/owncloud6/"
+//Server with webdav url
+#define webdavBaseUrl @"https://test1.owncloud.com/owncloud6/remote.php/webdav/"
+//Your user
+#define user @"ios-test" //@"username"
+//Your password
+#define password @"travis" //@"password"
+
+//Optional. You can change the folder of tests.
+#define pathTestFolder @"test"
