400ea220498798616a03f6845405a86d6c95b3b8
Gonzalo González <ggonzalez@solidgear.es>
2015-12-03 13:17:36 +0100

    Added horizontal grid view2

---
+++ OCLibraryExample/OCAppleTVExample/Cells/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  OCLibraryExample
+//
+//  Created by Gonzalo Gonzalez on 3/12/15.
+//  Copyright © 2015 ownCloud. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UICollectionViewCell {
+    
+    
+    func setSelectedStyle () {
+        
+        self.backgroundColor = UIColor.blueColor()
+    }
+    
+    func setUnselectedStyle () {
+        
+        self.backgroundColor = UIColor.blackColor()
+    }
+    
+   
+    
+    
+    
+}
