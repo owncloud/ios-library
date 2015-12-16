f2871dbbc8b68aa2d273fe3e40886afc275ef4a9
Javier Gonzalez <javiergonzper@gmail.com>
2015-12-16 10:55:09 +0100

    Added thre movies to be shown with all the information

---
+++ OCLibraryExample/OCAppleTVExample/Dtos/FilmsDto.swift
@@ -0,0 +1,36 @@
+//
+//  FilmsDto.swift
+//  OCLibraryExample
+//
+//  Created by Javier Gonzalez on 16/12/15.
+//  Copyright © 2015 ownCloud. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FilmsDto {
+    
+    var title:String?
+    var year:Int?
+    var rated:String?
+    var released:NSDate?
+    var runtime:String?
+    var genre:String?
+    var director:String?
+    var writer:String?
+    var actors:String?
+    var plot:String?
+    var language:String?
+    var country:String?
+    var awards:String?
+    var posterUrl:String?
+    var posterLocal:UIImage?
+    var metascore:Int?
+    var imdbRating:Float?
+    var imdbVotes:Int?
+    var imdbId:String?
+    var type:String?
+    var response:Bool?
+
+}
--- OCLibraryExample/OCAppleTVExample/ViewController.swift
+++ OCLibraryExample/OCAppleTVExample/ViewController.swift
@@ -10,13 +10,26 @@ import UIKit
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
-    let kMovieCellIdentifier: String = "MovieCellIdentifier"
+    let kMovieCellIdentifier:String = "MovieCellIdentifier"
     
-    @IBOutlet weak var descriptionLabel: UILabel?
+    //Movies
+    var moviesList:NSMutableArray?
+    
+    @IBOutlet weak var titleLabel: UILabel?
+    @IBOutlet weak var plotLabel: UILabel?
+    @IBOutlet weak var actorsLabel: UILabel?
+    @IBOutlet weak var directorLabel: UILabel?
+    @IBOutlet weak var runtimeLabel: UILabel?
+    @IBOutlet weak var yearLabel: UILabel?
+    @IBOutlet weak var posterImage: UIImageView?
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.moviesList = NSMutableArray()
+        self.createTheMoviesList()
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,13 +38,17 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 12
+        return 3
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell: MovieCell = collectionView.dequeueReusableCellWithReuseIdentifier(kMovieCellIdentifier, forIndexPath: indexPath) as! MovieCell
-        cell.backgroundColor = UIColor.blackColor()
-    
+
+        let currentMovie:FilmsDto = (self.moviesList?.objectAtIndex(indexPath.row))! as! FilmsDto //indexPath?.row
+        let currentPosterView = UIImageView(image: currentMovie.posterLocal)
+        currentPosterView.contentMode = UIViewContentMode.ScaleAspectFit
+        cell.backgroundView = currentPosterView
+        
     return cell
     }
     
@@ -41,6 +58,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
     
     func collectionView(collectionView: UICollectionView, didUnhighlightItemAtIndexPath indexPath: NSIndexPath) {
+        
         let cell: MovieCell = collectionView.cellForItemAtIndexPath(indexPath) as! MovieCell
         cell.setUnselectedStyle()
     }
@@ -88,11 +106,57 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     func collectionView(collectionView: UICollectionView, didUpdateFocusInContext context: UICollectionViewFocusUpdateContext, withAnimationCoordinator coordinator: UIFocusAnimationCoordinator) {
         
         print(context.nextFocusedIndexPath?.row)
-       
-        self.descriptionLabel?.text = String(context.nextFocusedIndexPath?.row)
+        
+        let currentMovie:FilmsDto = (self.moviesList?.objectAtIndex((context.nextFocusedIndexPath?.row)!))! as! FilmsDto
+        
+        self.titleLabel?.text = currentMovie.title
+        self.plotLabel?.text = currentMovie.plot
+        self.actorsLabel?.text = currentMovie.actors
+        self.directorLabel?.text = currentMovie.director
+        self.yearLabel?.text = String(currentMovie.year)
+        self.runtimeLabel?.text = currentMovie.runtime
+        self.posterImage?.image = currentMovie.posterLocal
        
     }
     
+    func createTheMoviesList() {
+        
+        let movie1:FilmsDto = FilmsDto()
+        movie1.title = "Raiders of the Lost Ark"
+        movie1.plot = "Archaeologist and adventurer Indiana Jones is hired by the US government to find the Ark of the Covenant before the Nazis."
+        movie1.actors = "Harrison Ford, Karen Allen, Paul Freeman, Ronald Lacey"
+        movie1.director = "Steven Spielberg"
+        movie1.runtime = "115 min"
+        movie1.year = 1981
+        movie1.posterLocal = UIImage(named: "movie1.jpg")
+        
+        self.moviesList?.addObject(movie1)
+        
+        let movie2:FilmsDto = FilmsDto()
+        movie2.title = "Indiana Jones and the Temple of Doom"
+        movie2.plot = "After arriving in India, Indiana Jones is asked by a desperate village to find a mystical stone. He agrees, and stumbles upon a secret cult plotting a terrible plan in the catacombs of an ancient palace."
+        movie2.actors = "Harrison Ford, Kate Capshaw, Jonathan Ke Quan, Amrish Puri"
+        movie2.director = "Steven Spielberg"
+        movie2.runtime = "118 min"
+        movie2.year = 1984
+        movie2.posterLocal = UIImage(named: "movie2.jpg")
+        
+        self.moviesList?.addObject(movie2)
+        
+        let movie3:FilmsDto = FilmsDto()
+        movie3.title = "Indiana Jones and the Last Crusade"
+        movie3.plot = "When Dr. Henry Jones Sr. suddenly goes missing while pursuing the Holy Grail, eminent archaeologist Indiana Jones must follow in his father's footsteps and stop the Nazis."
+        movie3.actors = "Harrison Ford, Sean Connery, Denholm Elliott, Alison Doody"
+        movie3.director = "Steven Spielberg"
+        movie3.runtime = "127 min"
+        movie3.year = 1989
+        movie3.posterLocal = UIImage(named: "movie3.jpg")
+        
+        self.moviesList?.addObject(movie3)
+        
+        
+        
+    }
 
     
 
