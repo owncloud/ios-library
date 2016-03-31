5e0f6a6b58071f1a91906ef437686dfbf22de244
Javier Gonzalez <javiergonzper@gmail.com>
2016-03-31 13:28:12 +0200

    Removed the version of the simulator

--- OCCommunicationLib/OCCommunicationLibTests/ConfigTests.m
+++ OCCommunicationLib/OCCommunicationLibTests/ConfigTests.m
@@ -40,6 +40,7 @@
     
     //We set the baseUrl
     if ([[[NSProcessInfo processInfo] environment] objectForKey:@"baseUrlTravis"]) {
+        
         _baseUrl = [[[NSProcessInfo processInfo] environment] objectForKey:@"baseUrlTravis"];
         NSLog(@"Environment variable exist: %@", _baseUrl);
     } else {
