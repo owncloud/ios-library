82391ff877413434c6fd8365eb69c7a19247c646
Javier Gonzalez <javiergonzper@gmail.com>
2016-04-01 13:39:16 +0200

    Removed the class ConfigTests and added just a .h ConfigTests to store the constans of the credentials

--- OCCommunicationLib/OCCommunicationLibTests/ConfigTests.h
+++ OCCommunicationLib/OCCommunicationLibTests/ConfigTests.h
@@ -2,41 +2,40 @@
 //  ConfigTests.h
 //  ownCloud iOS library
 //
-//  Created by Javier Gonzalez on 14/04/14.
-//  Copyright (c) 2014 ownCloud. All rights reserved.
+// Copyright (C) 2015 ownCloud Inc. (http://www.owncloud.org/)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 //
 
-#import <Foundation/Foundation.h>
-
-@interface ConfigTests : NSObject
-
-///-----------------------------------
-/// @name initWithVariables
-///-----------------------------------
-
-/**
- * Method init this object with the tests variables filled
- *
- * @return -> self object
- */
-- (id) initWithVariables;
-
-
 //Your entire server url. ex:https://example.owncloud.com/owncloud/
-@property (nonatomic, strong) NSString *baseUrl;
+#define k_base_url @""
 //Server with webdav url. ex: https://example.owncloud.com/owncloud/remote.php/webdav/
-@property (nonatomic, strong) NSString *webdavBaseUrl;
+#define k_webdav_base_url @""
 //Server user
-@property (nonatomic, strong) NSString *user;
+#define k_user @""
 //Server password
-@property (nonatomic, strong) NSString *password;
-//Optional. You can change the folder of tests.
-@property (nonatomic, strong) NSString *pathTestFolder;
+#define k_password @""
+//Optional. You can change the folder of tests
+#define k_path_test_folder @""
 //User to share
-@property (nonatomic, strong) NSString *userToShare;
+#define k_user_to_share @""
 //Group to share
-@property (nonatomic, strong) NSString *groupToShare;
-//Group to share
-@property (nonatomic, strong) NSString *remoteUserToShare;
-
-@end
+#define k_group_to_share @""
+//Remote user to share as federate
+#define k_remote_user_to_share @""
