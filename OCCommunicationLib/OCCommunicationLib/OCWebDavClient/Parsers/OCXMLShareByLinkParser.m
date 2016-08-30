936be60a8521c0d2c973a26e365c34afad23a33d
nasli <nalvarez@solidgear.es>
2016-08-30 12:51:42 +0200

    Update copyright

--- OCCommunicationLib/OCCommunicationLib/OCWebDavClient/Parsers/OCXMLShareByLinkParser.m
+++ OCCommunicationLib/OCCommunicationLib/OCWebDavClient/Parsers/OCXMLShareByLinkParser.m
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014 ownCloud. All rights reserved.
 //
 
-// Copyright (C) 2015 ownCloud Inc. (http://www.owncloud.org/)
+// Copyright (C) 2016, ownCloud GmbH. ( http://www.owncloud.org/ )
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
